559fbd7b0ba219405dc021120cbd72ba3c46dac3
aginies <aginies@suse.com>
2025-09-04 07:48:38 +0200

    simplify and fix issue with files creation

--- common/crtl_relay.py
+++ common/crtl_relay.py
@@ -17,18 +17,14 @@ last_ctrl_relay_time = 0
 relay1 = Pin(c_v.RELAY1_PIN, Pin.OUT, drive=Pin.DRIVE_3)
 relay2 = Pin(c_v.RELAY2_PIN, Pin.OUT, drive=Pin.DRIVE_3)
 
-def ctrl_relay(which_one, duration, adjust):
+def ctrl_relay(which_one, duration):
     """ relay 1 or 2, now non-blocking """
     lock.acquire()
 
     if which_one == 1:
         relay = relay1
-        active_file = '/BP1'
-        inactive_file = '/BP2'
     else:
         relay = relay2
-        active_file = '/BP2'
-        inactive_file = '/BP1'
 
     with open('/IN_PROGRESS', 'w') as file:
         file.write('This file was created by clicking BP1 or BP2.')
@@ -39,36 +35,21 @@ def ctrl_relay(which_one, duration, adjust):
         start_time = utime.time()
         last_print_time = start_time
         if not check_stop_relay():
-            if not adjust:
-                d_u.print_and_store_log("Full action in progress")
-                d_u.print_and_store_log(f"Creating {active_file}")
-                with open(active_file, 'w') as file:
-                    file.write(f'This file was created by clicking BP{which_one}.')
-                try:
-                    os.remove(inactive_file)
-                except OSError:
-                    pass
-                try:
-                    os.remove("/EMERGENCY_STOP")
-                except OSError:
-                    pass
-            else:
-                d_u.print_and_store_log("Adjustement in progress")
+            while utime.time() - start_time < duration:
+                if check_stop_relay():
+                    d_u.print_and_store_log(f"Stop requested for Relay {which_one} (duration left: {duration - (utime.time() - start_time):.1f}s).")
+                    relay.off()
+                    break
+                current_time = utime.time()
+                if current_time - last_print_time >= 5:
+                    elapsed_time = current_time - start_time
+                    d_u.print_and_store_log(f"Relay {which_one} has been ON for {elapsed_time:.1f} seconds. ({duration - elapsed_time:.1f}s remaining).")
+                    last_print_time = current_time
+                utime.sleep_ms(100)
 
-        while utime.time() - start_time < duration:
-            if check_stop_relay():
-                d_u.print_and_store_log(f"Stop requested for Relay {which_one} (duration left: {duration - (utime.time() - start_time):.1f}s).")
+            if relay.value() == 1:
+                d_u.print_and_store_log(f"Switching Relay {which_one} as Off")
                 relay.off()
-                break
-            current_time = utime.time()
-            if current_time - last_print_time >= 5:
-                elapsed_time = current_time - start_time
-                d_u.print_and_store_log(f"Relay {which_one} has been ON for {elapsed_time:.1f} seconds. ({duration - elapsed_time:.1f}s remaining).")
-                last_print_time = current_time
-            utime.sleep_ms(50)
-
-        if relay.value() == 1:
-            relay.off()
 
         else:
             # If stopped by emergency, ensure files are cleared
@@ -108,29 +89,26 @@ def thread_do_job_crtl_relay(B_text, relay_nb, duration):
     global last_ctrl_relay_time
     current_time = utime.time()
     response_content = ""
-    adjust = True
-
+    d_u.print_and_store_log(f"{current_time} {last_ctrl_relay_time}")
     if current_time - last_ctrl_relay_time > duration:
         d_u.print_and_store_log(f"{B_text} activé")
         last_ctrl_relay_time = current_time
-        if B_text != "OPEN_B" and B_text != "CLOSE_B":
-            adjust = False
-            d_u.print_and_store_log(f"Will Create /{B_text} file")
-        else:
-            adjust = True
-            d_u.print_and_store_log(f"Will Not Create any files")
         if B_text == "BP1":
             try:
+                with open("/BP1", 'w') as file:
+                    file.write(f'This file was created by clicking BP1.')
                 os.remove("/BP2")
             except OSError:
                 pass # BP2 might not exist
         elif B_text == "BP2":
             try:
+                with open("/BP2", 'w') as file:
+                    file.write(f'This file was created by clicking BP2.')
                 os.remove("/BP1")
             except OSError:
                 pass # BP1 might not exist
 
-        _thread.start_new_thread(ctrl_relay, (relay_nb, duration, adjust))
+        _thread.start_new_thread(ctrl_relay, (relay_nb, duration))
         response_content = B_text + " activated"
     else:
         d_u.print_and_store_log(f"{B_text} Duplicate request seen...")
