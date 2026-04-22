be7ac5d82c3caa78ffc1a2e7bf708b879d926d27
aginies <aginies@suse.com>
2026-04-23 00:01:42 +0200

    add link to stats and various small improvements

--- solar/web_command.py
+++ solar/web_command.py
@@ -2,11 +2,21 @@
 # GPL3
 
 import config_var as c_v
+import paths
 
 def create_html_response():
     try:
+        with open(paths.VERSION_FILE, 'r') as file:
+            version = file.read().strip()
+    except OSError:
+        version = "unknown"
+
+    try:
         with open('web_command.html', 'r') as f:
             tmpl = f.read()
-        return tmpl.format(EQUIPMENT_NAME=c_v.EQUIPMENT_NAME)
+        return tmpl.format(
+            EQUIPMENT_NAME=c_v.EQUIPMENT_NAME,
+            VERSION=version
+        )
     except OSError:
         return "Error: web_command.html not found."
